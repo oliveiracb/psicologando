5bec04d7c7b90730d63042356d8354011408278e
Aluno <Aluno@DESKTOP-3FIS8IH>
2019-06-17 08:07:34 -0300

    Inicial

---
+++ Psicologando/src/main/java/br/edu/ifpb/esperanca/daw2/dao/UsuarioDAO.java
@@ -0,0 +1,18 @@
+package br.edu.ifpb.esperanca.daw2.dao;
+
+import java.util.List;
+
+import br.edu.ifpb.esperanca.daw2.filtros.UsuarioFiltro;
+import br.edu.ifpb.esperanca.daw2.psicologando.entities.Usuario;
+
+public class UsuarioDAO extends DAO<Usuario> {
+	
+	public UsuarioDAO() {
+		super(Usuario.class);
+	}
+
+	public List<Usuario> findBy(UsuarioFiltro filtro) {
+		return null;
+	}
+	
+}
---
+++ Psicologando/src/main/java/br/edu/ifpb/esperanca/daw2/psicologando/entities/Identificavel.java
@@ -0,0 +1,9 @@
+package br.edu.ifpb.esperanca.daw2.psicologando.entities;
+
+public interface Identificavel {
+
+	public Long getId();
+	
+	public void setId(Long id);
+	
+}
