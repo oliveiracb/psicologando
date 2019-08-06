e1227b58fc6f4efeb5f0ff653cbf1bca18860f89
Andreza <andreza.santana@academico.ifpb.edu.br>
2019-08-06 10:25:37 -0300

    Testes e Interface

---
+++ Psicologando/src/main/java/br/edu/ifpb/esperanca/daw2/psicologando/interfaces/Psicologando.java
@@ -0,0 +1,34 @@
+package br.edu.ifpb.esperanca.daw2.psicologando.interfaces;
+
+	import br.edu.ifpb.esperanca.daw2.psicologando.entities.AlarmeMed;
+	import br.edu.ifpb.esperanca.daw2.psicologando.entities.MaterialApoio;
+	import br.edu.ifpb.esperanca.daw2.psicologando.entities.Mural;
+	import br.edu.ifpb.esperanca.daw2.psicologando.entities.Paciente;
+	import br.edu.ifpb.esperanca.daw2.psicologando.entities.PontoApoio;
+	import br.edu.ifpb.esperanca.daw2.psicologando.entities.Psicologo;
+	import br.edu.ifpb.esperanca.daw2.psicologando.entities.TerapiaAdj;
+	import br.edu.ifpb.esperanca.daw2.psicologando.entities.Usuario;
+
+	public interface Psicologando {
+
+	public Usuario cadastrarUsu(Usuario usuario);
+	public void acessarMaterialDeApoio(Paciente paciente, MaterialApoio materialDeApoio);
+	public MaterialApoio postarMaterial(Psicologo psicologo, MaterialApoio material);
+	public Mural addRelato(Paciente paciente, Mural muralRelatar);
+	public void acessarRelato(Usuario usuario, Mural muralRelatar);
+	public Mural enviarRelato(Paciente paciente, Mural muralRelato);
+	public Psicologo editarMural(Psicologo psicologo, Mural editarRelatos );
+	public AlarmeMed addHoraMedicamento(Paciente paciente, AlarmeMed addHoraMed);
+	public void alarmarMed( Paciente paciente, AlarmeMed alarmarParaTomarMed  );  
+	public PontoApoio indicarPontoMaisProximo(PontoApoio pontoApoio, Psicologo psicologo);
+	public TerapiaAdj addTerapia(TerapiaAdj TerapiaAdjuvante, Psicologo indicaTerapia);
+	public void acessarTerapiaAdj(Paciente paciente, TerapiaAdj visualizarMApoio);
+	
+
+
+
+
+
+	}
+
+
